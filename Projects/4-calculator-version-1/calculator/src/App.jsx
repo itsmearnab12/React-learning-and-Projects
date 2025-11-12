92b9141b490d30182d87158cbe27c2a60049e2e0
Arnab <arnabchoudhury2005@gmail.com>
2025-11-12 23:07:47 +0530

    Working on project

--- Projects/4-calculator-version-1/calculator/src/App.jsx
+++ Projects/4-calculator-version-1/calculator/src/App.jsx
@@ -1,16 +1,16 @@
-import Display from './components/Display'
 import styles from './App.module.css'
-import ButtonsContainer from './components/ButtonContainer'
 
 function App() {
-  
-  return (
-  <div className={styles.calculator}>
-    <Display></Display>
-    <ButtonsContainer></ButtonsContainer>
+
+  return <div className={styles.calculator}>
+    <input className={styles.display} type="text" />
+    <div className={styles.buttonsContainer}>
+      <button className={styles.button}>C</button>
+      <button className={styles.button}>1</button>
+      <button className={styles.button}>2</button>
+      <button className={styles.button}>+</button>
+    </div>
   </div>
-  )
- 
 }
 
 export default App
